db9f276392bc6b0cfdb97cf2d4bed22b1ed15dee
EvanLiu <296823596@qq.com>
2018-09-23 20:42:06 +0800

    update param.js

--- common/param.js
+++ common/param.js
@@ -1,7 +1,6 @@
 /*
  * 搜索参数 => 增、删、改、查、格式化
  */
-import { searchMode } from './Config'
 
 const valueSeparator=',';
 
@@ -12,16 +11,16 @@ const valueSeparator=',';
  * @param {JSON} map 
  * @returns {String}
  */
-function concatSearch(search,map){
-  var searchMap=formatSearch(search);
+function concatSearch(search, map){
+  var searchMap = formatSearch(search);
   for(let key in map){
-    if(searchMap[key]&&searchMap[key].split(valueSeparator).some((v)=>v==map[key])) continue;//重复参数忽略
+    if(searchMap[key]&&searchMap[key].split(valueSeparator).some((v) => v==map[key])) continue;//重复参数忽略
     if(searchMap[key]){
-      let arr=searchMap[key].split(valueSeparator);
+      let arr = searchMap[key].split(valueSeparator);
       arr.push(map[key])
-      searchMap[key]=arr.join(valueSeparator)
+      searchMap[key] = arr.join(valueSeparator)
     }else{
-      searchMap[key]=map[key]+'' // 防止提交数字格式
+      searchMap[key] = map[key]+'' // 防止提交数字格式
     }
   }
   return joinSearch(searchMap)
@@ -35,30 +34,30 @@ function concatSearch(search,map){
 function formatSearch(search){
   var searchMap = {};
   if(!search){ return searchMap }
-  searchMap=search.replace(/^\?/,'').split('&').reduce(function(acc,v,i,a){
-    let key=v.split('=')[0],value=v.split('=')[1];
+  searchMap = search.replace(/^\?/,'').split('&').reduce(function(acc,v,i,a){
+    let key = v.split('=')[0], value = v.split('=')[1];
     if(acc[key]){
-      let arr=acc[key].split(valueSeparator)
+      let arr = acc[key].split(valueSeparator)
       arr.push(value)
-      acc[key]=arr.join(valueSeparator)
+      acc[key] = arr.join(valueSeparator)
     }else{
-      acc[key]=value+'' // 防止提交数字格式
+      acc[key] = value+'' // 防止提交数字格式
     }
     return acc
-  },{})
+  }, {})
   return searchMap
 }
 function joinSearch(searchMap){
-  var search=[];
+  var search = [];
   for(let key in searchMap){
-    searchMap[key].split(valueSeparator).forEach((v)=>{
+    searchMap[key].split(valueSeparator).forEach((v) => {
       if(v){
-        search.push(key+'='+v)
+        search.push(key + '='+v)
       }
     })
   }
-  if(search.length>0){
-    return '?'+search.join('&')
+  if( search.length > 0 ){
+    return '?' + search.join('&')
   }else{
     return ''
   }
@@ -70,13 +69,13 @@ function joinSearch(searchMap){
  * @param {JSON} map 
  * @returns {String}
  */
-function setSearchParam(search,map){
-  var searchMap=formatSearch(search);
+function setSearchParam(search, map){
+  var searchMap = formatSearch(search);
   for(let key in map){
-    if(map[key]===0 || map[key]==='0' || map[key]){ //0 '0'是允许格式参数(价格范围:0~50)
-      searchMap[key]=map[key]+'' // 防止提交数字格式
+    if(map[key] === 0 || map[key]=== '0' || map[key]){ //0 '0'是允许格式参数(价格范围:0~50)
+      searchMap[key] = map[key]+'' // 防止提交数字格式
     }else{
-      searchMap[key]=''
+      searchMap[key] = ''
     }
   }
   return joinSearch(searchMap)
@@ -87,8 +86,8 @@ function setSearchParam(search,map){
  * @param {String || Array} key 
  * @returns {JSON}
  */
-function removeSearchParam(search,key){
-  var searchMap=formatSearch(search);
+function removeSearchParam(search, key){
+  var searchMap = formatSearch(search);
   if(typeof key == 'string'){
     delete searchMap[key]
   }else if(Array.isArray(key)){
@@ -107,7 +106,7 @@ function removeSearchParam(search,key){
  * @param {String} key 
  * @returns {String} value
  */
-function getSearchParam(search,key){
+function getSearchParam(search, key){
   var searchMap=formatSearch(search);
   if(searchMap[key]){
     return searchMap[key]
@@ -123,37 +122,18 @@ function getSearchParam(search,key){
  * @returns 
  */
 function formatPath(level){
-  level=level?parseInt(level):1;//基路由级数，默认为一级目录
+  level = level ? parseInt(level) : 1;//基路由级数，默认为一级目录
   if(typeof window === 'undefined'){
     return false
   }
   var paramArr=window.location.pathname.split('/');
   return {
-    // name:paramArr[level],
-    cat:paramArr[level+1],
-    title:paramArr[level+2],
-    id:paramArr[level+3],
-    dest:paramArr[level+4]
+    // name: paramArr[level],
+    cat: paramArr[level+1],
+    title: paramArr[level+2],
+    id: paramArr[level+3],
+    dest: paramArr[level+4]
   }
-}
-
-
-/**
- * 是否是客户端
- * 
- * @returns {Boolean}
- */
-function isClient(){
-  return typeof window !== 'undefined'
-}
-
-/**
- * 是否是node端
- * 
- * @returns {Boolean}
- */
-function isNode(){
-  return typeof module !== "undefined" && typeof module.exports !== "undefined"
 }
 
 /**
@@ -162,14 +142,15 @@ function isNode(){
  * @param {String} url:window.location.href
  */
 function pushState(url){
-  if(searchMode === 'async'){
-    window.history.pushState({action:'research'},'title',url)
+  if( window.history.pushState ){
+    window.history.pushState({action:'research'}, 'title',url)
     //触发自定义pushState事件
-    // const e = new Event("pushState");
-    // window.dispatchEvent(e);
-    $(window).trigger('pushState',{action:'research'})
+    if(CustomEvent && window.dispatchEvent){
+      const e = new CustomEvent("pushState");
+      window.dispatchEvent(e);
+    }
   }else{
-    window.location.href=url
+    window.location.href = url
   }
 }
 
@@ -201,7 +182,5 @@ module.exports = {
   formatPath,
   pushState,
   serializeParam,
-  getUrlParam,
-  isClient,
-  isNode,
+  getUrlParam
 }
